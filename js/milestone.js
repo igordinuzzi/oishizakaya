36a0a49f909dc275bd9d6660755f253b401167a8
Igor Dinuzzi <75853437+igordinuzzi@users.noreply.github.com>
2023-11-02 19:06:56 +0100

    Add: open button to MILESTONE JS

--- js/milestone.js
+++ js/milestone.js
@@ -2,33 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const milestones = document.querySelectorAll('.milestone');
 
     milestones.forEach((milestone) => {
-        const closeButton = milestone.querySelector('.close-button');
+        const milestoneTitle = milestone.querySelector('.milestone-toggle');
+        const toggleButton = milestone.querySelector('.milestone-toggle-button');
         const details = milestone.querySelector('.milestone-details');
 
-        closeButton.style.display = 'none'; // Initially hide the close button
-
-        closeButton.addEventListener('click', () => {
-            details.style.display = 'none';
-            closeButton.style.display = 'none';
-        });
-
-        milestone.addEventListener('click', () => {
-            const isActive = milestone.classList.contains('active');
-
-            milestones.forEach((m) => {
-                m.classList.remove('active');
-                const closeBtn = m.querySelector('.close-button');
-                if (closeBtn) {
-                    closeBtn.style.display = 'none';
-                }
-            });
-
-            if (!isActive) {
-                milestone.classList.add('active');
-                closeButton.style.display = 'block'; // Show the close button when the milestone is active
+        toggleButton.addEventListener('click', () => {
+            const isClosed = details.style.display === 'none' || details.style.display === '';
+            
+            if (isClosed) {
+                details.style.display = 'block';
+                toggleButton.textContent = 'Close';
+            } else {
+                details.style.display = 'none';
+                toggleButton.textContent = 'Open';
             }
-            // Toggle the visibility of the milestone details content
-            details.style.display = isActive ? 'none' : 'block';
         });
     });
 });
